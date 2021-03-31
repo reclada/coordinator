2f1630e69e3cf44526d507479eced31ce400078b
Ivan Kolesnikov <ivan.kolesnikov@Ivans-MacBook-Pro.local>
2021-03-31 17:17:49 +0300

    Fix luigi unfulfilled dependency error

--- reclada/coordinator/parallel_execution/luigi_executor.py
+++ reclada/coordinator/parallel_execution/luigi_executor.py
@@ -9,11 +9,12 @@ logger = logging.getLogger("luigi-interface." + __name__)
 
 
 class GenerateData(luigi.Task):
+    out_prefix = luigi.Parameter()
     numbers_amount = luigi.IntParameter()  # type: int
     generate_sleep = luigi.FloatParameter(default=0)  # type: float
 
     def output(self):
-        out_file = f'{time.strftime("%Y-%m-%d_%H-%M-%S")}_luigi_test_data.txt'
+        out_file = f'{self.out_prefix}_data.txt'
         return luigi.LocalTarget(out_file)
 
     def run(self):
@@ -24,15 +25,16 @@ class GenerateData(luigi.Task):
 
 
 class CalcSum(luigi.Task):
+    out_prefix = luigi.Parameter()
     numbers_amount = luigi.IntParameter()  # type: int
     generate_sleep = luigi.FloatParameter(default=0)  # type: float
     calc_sleep = luigi.FloatParameter(default=0)  # type: float
 
     def requires(self):
-        return GenerateData(self.numbers_amount, self.generate_sleep)
+        return GenerateData(self.out_prefix, self.numbers_amount, self.generate_sleep)
 
     def output(self):
-        out_file = f'{time.strftime("%Y-%m-%d_%H-%M-%S")}_luigi_test_calculation_sum.txt'
+        out_file = f'{self.out_prefix}.txt'
         return luigi.LocalTarget(out_file)
 
     def run(self):
@@ -45,10 +47,6 @@ class CalcSum(luigi.Task):
 
 
 class CalcAmount(CalcSum):
-    def output(self):
-        out_file = f'{time.strftime("%Y-%m-%d_%H-%M-%S")}_luigi_test_calculation_amount.txt'
-        return luigi.LocalTarget(out_file)
-
     def run(self):
         with self.input().open('r') as infile:
             data = infile.read().splitlines()
@@ -59,23 +57,31 @@ class CalcAmount(CalcSum):
 
 
 class SleepTask(luigi.Task):
+    task_complete = False
     sleep_time = luigi.FloatParameter(default=0)  # type: float
 
     def run(self):
         time.sleep(self.sleep_time)
+        self.task_complete = True
+
+    def complete(self):
+        return self.task_complete
 
 
 if __name__ == '__main__':
     FIRST_TASK_EXECUTION = 2
     SECOND_TASK_EXECUTION = 3
-    start = time.time()
-    luigi.build(
-        [
-            CalcSum(100, FIRST_TASK_EXECUTION, SECOND_TASK_EXECUTION),
-            CalcAmount(200, FIRST_TASK_EXECUTION, SECOND_TASK_EXECUTION),
-            SleepTask(FIRST_TASK_EXECUTION)
-        ],
-        local_scheduler=True,
-        workers=3
-    )
-    logger.debug(f"Execution time: {time.time() - start}")
+    ITERATION_AMOUNT = 2
+    for _ in range(ITERATION_AMOUNT):
+        cur_ts = time.strftime("%Y-%m-%d_%H-%M-%S")
+        start = time.time()
+        luigi.build(
+            [
+                CalcSum(f'{cur_ts}_calc_sum', 100, FIRST_TASK_EXECUTION, SECOND_TASK_EXECUTION),
+                CalcAmount(f'{cur_ts}_calc_amount', 200, FIRST_TASK_EXECUTION, SECOND_TASK_EXECUTION),
+                SleepTask(FIRST_TASK_EXECUTION)
+            ],
+            local_scheduler=True,
+            workers=3
+        )
+        logger.debug(f"Execution time: {time.time() - start}")
